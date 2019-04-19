892d7d4a22d059b247d662ce8936837fbee22d2d
seleregb <seleregb@gmail.com>
2019-04-18 23:21:52 -0400

    completed anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,32 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let strA = cleanString(stringA);
+  let strB = cleanString(stringB);
+
+  //   return JSON.stringify(buildCharMap(strA)) === JSON.stringify(buildCharMap(strB));
+  return strA === strB;
+  // console.log(JSON.stringify(strA_Map) === JSON.stringify(strB_Map), strA_Map, strB_Map);
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
