33b7e55e37f12503582199d767db176011e2bef2
seleregb <seleregb@gmail.com>
2019-04-19 21:02:43 -0400

    completed pyramid exercise in iteration & recrusion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,53 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+/**
+ * recursive solution
+ * @param {number} n
+ */
+function pyramid(n, row = 0, level = "") {
+  if (n === row) {
+    // at the last row
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    // hit the end of a row
+    console.log(level);
+    pyramid(n, row + 1); // go to the next row
+    return;
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+
+  pyramid(n, row, level);
+}
 
 module.exports = pyramid;
+
+/**
+ * @param {number} n
+ * iterative solution
+ */
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       // make sure that the current column is within the bounds of the midpoint
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
